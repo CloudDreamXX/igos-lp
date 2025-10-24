fb2b42dd19cdd441c650d3ce1c9db2c205848ed4
CloudDreamXX <clouddreamxx@gmail.com>
2025-10-24 18:33:00 +0300

    contact form

--- src/widgets/HeroSection/ContactUs.tsx
+++ src/widgets/HeroSection/ContactUs.tsx
@@ -20,7 +20,7 @@ export const ContactUs: React.FC<Props> = ({ onClose }) => {
                 <div className="flex gap-4">
                     <input
                         type="text"
-                        placeholder="Name"
+                        placeholder="First Name"
                         className="w-1/2 border border-gray-300 rounded-md px-4 py-2 outline-none" />
                     <input
                         type="text"
